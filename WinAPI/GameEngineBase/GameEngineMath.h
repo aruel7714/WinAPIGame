1424b41bea8af2739966d04c395a40817b18de27
aruel7714 <harunia7714@daum.net>
2023-05-13 20:58:53 +0900

    GameEngineBase 생성

---
+++ WinAPI/GameEngineBase/GameEngineMath.h
@@ -0,0 +1,148 @@
+#pragma once
+
+// Ό³Έν : 
+class GameEngineMath
+{
+};
+
+class float4
+{
+public:
+	static const float4 ZERO;
+
+	static const float4 LEFT;
+	static const float4 RIGHT;
+	static const float4 UP;
+	static const float4 DOWN;
+
+	float X = 0.0f;
+	float Y = 0.0f;
+	float Z = 0.0f;
+	float W = 1.0f;
+
+	inline int iX() const
+	{
+		return static_cast<int>(X);
+	}
+
+	inline int iY() const
+	{
+		return static_cast<int>(Y);
+	}
+
+	inline float hX() const
+	{
+		return X * 0.5f;
+	}
+
+	inline float hY() const
+	{
+		return Y * 0.5f;
+	}
+
+	inline float ihX() const
+	{
+		return static_cast<int>(hX());
+	}
+
+	inline float ihY() const
+	{
+		return static_cast<int>(hY());
+	}
+
+	inline float4 Half() const
+	{
+		return { hX(), hY(), Z, W };
+	}
+
+	float4 operator-()
+	{
+		float4 ReturnValue = *this;
+
+		ReturnValue.X = -ReturnValue.X;
+		ReturnValue.Y = -ReturnValue.Y;
+		ReturnValue.Z = -ReturnValue.Z;
+
+		return ReturnValue;
+	}
+
+	float4 operator-(const float4& _Other)
+	{
+		float4 ReturnValue;
+
+		ReturnValue.X = X - _Other.X;
+		ReturnValue.Y = Y - _Other.Y;
+		ReturnValue.Z = Z - _Other.Z;
+
+		return ReturnValue;
+	}
+
+	float4 operator+(const float4& _Other)
+	{
+		float4 ReturnValue;
+
+		ReturnValue.X = X + _Other.X;
+		ReturnValue.Y = Y + _Other.Y;
+		ReturnValue.Z = Z + _Other.Z;
+
+		return ReturnValue;
+	}
+
+	float4 operator*(const float4& _Other)
+	{
+		float4 ReturnValue;
+
+		ReturnValue.X = X * _Other.X;
+		ReturnValue.Y = Y * _Other.Y;
+		ReturnValue.Z = Z * _Other.Z;
+
+		return ReturnValue;
+	}
+
+	float4 operator*(const float _Value)
+	{
+		float4 ReturnValue;
+
+		ReturnValue.X = X * _Value;
+		ReturnValue.Y = Y * _Value;
+		ReturnValue.Z = Z * _Value;
+
+		return ReturnValue;
+	}
+
+	float4& operator+=(const float4& _Other)
+	{
+		X += _Other.X;
+		Y += _Other.Y;
+		Z += _Other.Z;
+
+		return *this;
+	}
+
+	float4& operator-=(const float4& _Other)
+	{
+		X -= _Other.X;
+		Y -= _Other.Y;
+		Z -= _Other.Z;
+
+		return *this;
+	}
+
+	float4& operator*=(const float4& _Other)
+	{
+		X *= _Other.X;
+		Y *= _Other.Y;
+		Z *= _Other.Z;
+
+		return *this;
+	}
+
+	float4& operator*=(const float _Value)
+	{
+		X *= _Value;
+		Y *= _Value;
+		Z *= _Value;
+
+		return *this;
+	}
+};
---
+++ WinAPI/GameEngineBase/GameEnginePath.cpp
@@ -0,0 +1,82 @@
+#include "GameEnginePath.h"
+#include "GameEngineDebug.h"
+
+GameEnginePath::GameEnginePath()
+{
+}
+
+GameEnginePath::GameEnginePath(const std::string& _Path)
+	:Path(_Path)
+{
+}
+
+GameEnginePath::~GameEnginePath()
+{
+}
+
+std::string GameEnginePath::GetFileName()
+{
+	return Path.filename().string();
+}
+
+void GameEnginePath::GetCurrentPath()
+{
+	Path = std::filesystem::current_path();
+}
+
+void GameEnginePath::MoveParent()
+{
+	Path = Path.parent_path();
+}
+
+void GameEnginePath::MoveParentToExistsChild(const std::string& _ChildPath)
+{
+	while (true)
+	{
+		std::filesystem::path CheckPath = Path;
+
+		CheckPath.append(_ChildPath);
+
+		if (false == std::filesystem::exists(ChecPath))
+		{
+			MoveParent();
+		}
+		else
+		{
+			break;
+		}
+
+		if (Path == Path.root_path(0))
+		{
+			MsgBoxAssert("루트 경로까지 샅샅히 뒤졌지만 " + _ChildPath + "파일이나 폴더를 하위로 가지고 있는 경로를 찾지 못했습니다.");
+		}
+	}
+}
+
+void GameEnginePath::MoveChild(const std::string& _ChildPath)
+{
+	std::filesystem::path CheckPath = Path;
+
+	CheckPath.append(_ChildPath);
+
+	if (false == std::filesystem::exists(CheckPath))
+	{
+		MsgBoxAssert("존재하지 않는 경로로 이동하려고 했습니다." + CheckPath.string());
+	}
+
+	Path = CheckPath;
+}
+
+std::string GameEnginePath::PlusFilePath(const std::string& _ChildPath)
+{
+	std::filesystem::path CheckPath = Path;
+
+	CheckPath.append(_ChildPath);
+
+	if (false == std::filesystem::exists(CheckPath))
+	{
+		MsgBoxAssert("존재하지 않는 경로로 이동하려고 했습니다." + CheckPath.string());
+	}
+
+	return CheckPath.string();
+}
