db6a52d5dd02bdf0e7762b228a1c97f2856b6cf0
aruel7714 <harunia7714@daum.net>
2023-05-14 18:47:17 +0900

    GameEnginePlatform 생성

---
+++ WinAPI/GameEnginePlatform/GameEngineWindow.cpp
@@ -0,0 +1,195 @@
+#include "GameEngineWindow.h"
+#include <GameEngineBase/GameEngineDebug.h>
+
+HINSTANCE GameEngineWindow::Instance = nullptr;
+
+GameEngineWindow GameEngineWindow::MainWindow;
+
+bool GameEngineWindow::IsWindowUpdate = true;
+bool GameEngineWindow::IsFocusValue = false;
+
+GameEngineWindow::GameEngineWindow()
+{
+}
+
+GameEngineWindow::~GameEngineWindow()
+{
+}
+
+void GameEngineWindow::ClearBackBuffer()
+{
+	Rectangle(BackBuffer->GetImageDC(), 0, 0, BackBuffer->GetScale().iX(), BackBuffer->GetScale().iY());
+}
+
+void GameEngineWindow::DoubleBuffering()
+{
+	WindowBuffer->BitCopy(BackBuffer, Scale.Half(), BackBuffer->GetScale());
+}
+
+void GameEngineWindow::Open(const std::string& _Title, HINSTANCE _hInstance)
+{
+	Instance = _hInstance;
+	Title = _Title;
+
+	if (nullptr == Instance)
+	{
+		MsgBoxAssert("HInstance없이 윈도우를 만들수는 없습니다.");
+		return;
+	}
+
+	MyRegisterClass();
+	InitInstance();
+}
+
+void GameEngineWindow::InitInstance()
+{
+	hWnd = CreateWindowA(
+		"DefalutWindow",
+		Title.c_str(),
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		0,
+		CW_USEDEFAULT,
+		0,
+		nullptr,
+		nullptr,
+		Instance,
+		nullptr
+	);
+
+	if (!hWnd)
+	{
+		MsgBoxAssert("윈도우 생성에 실패했습니다.");
+		return;
+	}
+
+	Hdc = ::GetDC(hWnd);
+
+	WindowBuffer = new GameEngineWindowTexture();
+	WindowBuffer->ResCreate(Hdc);
+	
+	BackBuffer = new GameEngineWindowTexture();
+	BackBuffer->ResCreate(WindowBuffer->GetScale());
+
+	ShowWindow(hWnd, SW_SHOW);
+
+	UpdateWindow(hWnd);
+}
+
+LRESULT CALLBACK GameEngineWindow::WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+	case WM_SETFOCUS:
+	{
+		IsFocusValue = true;
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	case WM_KILLFOCUS:
+	{
+		IsFocusValue = false;
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+	}
+	break;
+	case WM_DESTROY:
+		IsWindowUpdate = false;
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+}
+
+void GameEngineWindow::MyRegisterClass()
+{
+	static bool Check = false;
+
+	if (true == Check)
+	{
+		return;
+	}
+
+	WNDCLASSEXA wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = GameEngineWindow::WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = Instance;
+	wcex.hIcon = nullptr;
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 2);
+	wcex.lpszMenuName = nullptr;
+	wcex.lpszClassName = "DefaultWindow";
+	wcex.hIconSm = nullptr;
+
+	if (false == RegisterClassExA(&wcex))
+	{
+		MsgBoxAssert("윈도우 클래스 등록에 실패했습니다.");
+		return;
+	}
+	Check = true;
+}
+
+void GameEngineWindow::MessageLoop(HINSTANCE _Inst, void(*_Start)(HINSTANCE), void(*_Update)(), void(*_End)())
+{
+	if (nullptr != _Start)
+	{
+		_Start(_Inst);
+	}
+
+	MSG msg;
+
+	while (IsWindowUpdate)
+	{
+		if (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			if (nullptr != _Update)
+			{
+				_Update();
+			}
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			continue;
+		}
+
+		if (nullptr != _Update)
+		{
+			_Update();
+		}
+
+	}
+
+	if (nullptr != _End)
+	{
+		_End();
+	}
+
+	return;
+}
+
+void GameEngineWindow::SetPosAndScale(const float4& _Pos, const float4& _Scale)
+{
+	Scale = _Scale;
+
+	if (nullptr != BackBuffer)
+	{
+		delete BackBuffer;
+		BackBuffer = new GameEngineWindowTexture();
+		BackBuffer->ResCreate(Scale);
+	}
+
+	RECT Rc = { 0, 0, _Scale.iX(), _Scale.iY() };
+
+	AdjustWindowRect(&Rc, WS_OVERLAPPEDWINDOW, FALSE);
+
+	SetWindowPos(hWnd, nullptr, _Pos.iX(), _Pos.iY(), Rc.right - Rc.left, Rc.bottom - Rc.top, SWP_NOZORDER);
+}
---
+++ WinAPI/GameEnginePlatform/GameEngineWindow.h
@@ -0,0 +1,84 @@
+#pragma once
+#include <Windows.h>
+#include <string>
+#include "GameEngineWindowTexture.h"
+
+// Ό³Έν : 
+class GameEngineWindow
+{
+public:
+	static GameEngineWindow MainWindow;
+
+	// constrcuter destructer
+	GameEngineWindow();
+	~GameEngineWindow();
+
+	// delete Function
+	GameEngineWindow(const GameEngineWindow & _Other) = delete;
+	GameEngineWindow(GameEngineWindow && _Other) noexcept = delete;
+	GameEngineWindow& operator=(const GameEngineWindow & _Other) = delete;
+	GameEngineWindow& operator=(GameEngineWindow && _Other) noexcept = delete;
+
+	void Open(const std::string& _Title, HINSTANCE _hInstance);
+
+	static void MessageLoop(HINSTANCE _Inst, void(*_Start)(HINSTANCE), void(*Update)(), void(*End)());
+
+	HDC GetHDC()
+	{
+		return Hdc;
+	}
+
+	float4 GetScale()
+	{
+		return Scale;
+	}
+
+	GameEngineWindowTexture* GetWindowBuffer()
+	{
+		return WindowBuffer;
+	}
+
+	GameEngineWindowTexture* GetBackBuffer()
+	{
+		return BackBuffer;
+	}
+
+	void SetPosAndScale(const float4& _Pos, const float4& _Scale);
+
+	static void WindowLoopOff()
+	{
+		IsWindowUpdate = false;
+	}
+
+	void ClearBackBuffer();
+	void DoubleBuffering();
+
+	static bool IsFocus()
+	{
+		return IsFocusValue;
+	}
+
+protected:
+
+private:
+	static bool IsWindowUpdate;
+
+	static HINSTANCE Instance;
+	std::string Title = "";
+
+	HWND hWnd = nullptr;
+
+	static bool IsFocusValue;
+
+	float4 Scale;
+
+	GameEngineWindowTexture* WindowBuffer = nullptr;
+	GameEngineWindowTexture* BackBuffer = nullptr;
+
+	HDC Hdc = nullptr;
+
+	static LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	void InitInstance();
+	void MyRegisterClass();
+};
+
